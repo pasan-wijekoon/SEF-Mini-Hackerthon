0bdfcac4e1622a57c8bda3cb05d15f701dbf5128
Ransidu Subasinghe <ransidu2003@gmail.com>
2026-09-04 10:47:45 +0530

    dns server update code

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 
